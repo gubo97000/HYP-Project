6ce73b4aaae961eb2e76620f8cfd9c8343070b7d
gubo97000 <gubo97000@hotmail.it>
2022-05-10 00:51:51 +0200

    some enanchement ,  try /api/graphiql to test the API

--- postgraphileServerMiddleware.ts
+++ postgraphileServerMiddleware.ts
@@ -1,8 +1,29 @@
 // const { postgraphile } = require('postgraphile')
-import {postgraphile}from 'postgraphile'
-module.exports = postgraphile(process.env.DATABASE_URL, 'public', {
+import { postgraphile, PostGraphileOptions } from 'postgraphile'
+const postgraphileOptions :PostGraphileOptions= {
+  subscriptions: true,
   watchPg: true, // automatic reload when database changes
+  dynamicJson: true,
+  setofFunctionsContainNulls: false,
+  ignoreRBAC: false,
+  showErrorStack: "json",
+  extendedErrors: ["hint", "detail", "errcode"],
+  appendPlugins: [require("@graphile-contrib/pg-simplify-inflector")],
+  exportGqlSchemaPath: "schema.graphql",
   graphiql: true, // for dev
+  enhanceGraphiql: true,
+  // allowExplain(req) {
+  //   // TODO: customise condition!
+  //   return true;
+  // },
+  enableQueryBatching: true,
+  legacyRelations: "omit",
+  // pgSettings(req) {
+  //   /* TODO */
+  // },
+};
+module.exports = postgraphile(process.env.DATABASE_URL, 'public', {
+  ...postgraphileOptions,
   graphqlRoute: '/api/graphql', // optional to avoid conflicts with nuxt.js default routing
-  graphiqlRoute: '/api/graphiql'
+  graphiqlRoute: '/api/graphiql',
 })
