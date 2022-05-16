f1ba2eb6bb53e827057435f1f00eb6afea4493d4
Alessandro Atanassov <alessandro.atanassov@mail.polimi.it>
2022-05-16 16:05:48 +0200

    Breadcrumb component

---
+++ components/Breadcrumb.vue
@@ -0,0 +1,81 @@
+    <!-- <nav>
+          <ol class="breadcrumb">
+          <li
+            v-for="(crumb, ci) in crumbs" :key="ci"
+            class="breadcrumb-item align">
+            <button class="btn btn-link" :class="{ disabled: isLast(ci) }" @click="selected(crumb)">
+                {{ crumb }}
+            </button>
+          </li>
+      </ol>
+    </nav> -->
+
+
+  <!-- <nav class="bg-grey-light rounded-md w-full">
+      <ol class="flex breadcrumb">
+          <li
+            v-for="(crumb, ci) in crumbs" :key="ci"
+            class=" align">
+            <button :class="isLast(ci) ? { disabled: true } : 'text-blue-600 hover:text-blue-700'" @click="selected(crumb)">
+                {{ crumb }}
+                <span v-if="!isLast(ci)" class="text-gray-500 mx-2">></span>
+            </button>
+          </li>
+      </ol>
+  </nav> -->
+
+
+<template>
+    <nav>
+        <ol class="breadcrumb">
+            <div v-for="(crumb, ci) in crumbs" :key="ci" class="crumb-item">
+                <li>
+                    <button class="btn btn-link hover:text-blue-300" :class="{ disabled: isLast(ci) }" @click="selected(crumb)">
+                        {{ crumb }}
+                    </button>
+                </li>
+                <li>
+                    <button v-if="!isLast(ci)" disabled="isLast(ci)" class="separator" >
+                        {{ "&#187;" }}
+                    </button>
+                </li>
+            </div>
+        </ol>
+    </nav>
+</template>
+
+<style scoped>
+.breadcrumb {
+    background-color: white;
+    border: 1px solid rgba(0,0,0,0.125);
+    border-radius: 0.37rem;
+    width: auto;
+}
+.separator {
+    font-size: 22px;
+    color: #6c757d;
+}
+.crumb-item {
+    display: flex;
+}
+</style>
+
+<script>
+export default {
+  name: 'Breadcrumb',
+  props: {
+      crumbs: {
+          type: Array,
+          required: true
+      }
+  },
+  methods: {
+      isLast(index) {
+          return index === this.crumbs.length - 1;
+      },
+      selected(crumb) {
+          this.$emit('selected', crumb);
+      }
+  }
+}
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,32 +1,29 @@
 <template>
   <div class="page container mt-5">
     <h1 class="display-4">About us</h1>
-    <div class="row mt-3">
-      <card
-        v-for="(cat, catIndex) of catList"
-        class="col-sm-2 m-2"
-        :key="`cat-index-${catIndex}`"
-        :id="cat.id"
-        :name="cat.name"
-        :img="cat.img"
-        :breed="cat.breed"
-      />
-    </div>
+    <Breadcrumb class="row justify-items-center mt-4" :crumbs="crumbs" @selected="selected"/>
+    <br>
   </div>
 </template>
 
 <script>
-import Card from '~/components/Card.vue'
+import Breadcrumb from '~/components/Breadcrumb.vue'
 export default {
-  name: 'ListPage',
+  name: 'AboutPage',
   components: {
-    Card,
+    Breadcrumb
   },
   data() {
     return {
-      // catList: []
+      crumbs: ['Home', 'About']
     }
   },
+  methods: {
+    selected(crumb) {
+      console.log(crumb);
+    }
+  },
+
   // Note: This happens on backend (server) side
   // async asyncData({ $axios }) {
   //   // const { data } = await $axios.get('http://localhost:3000/api/cats')
