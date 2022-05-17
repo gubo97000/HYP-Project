8048f4281da6b8cf93fc657257d90e230a2981fc
Alessandro Atanassov <alessandro.atanassov@mail.polimi.it>
2022-05-17 11:26:56 +0200

    Add content to Event details page

--- pages/events/_id.vue
+++ pages/events/_id.vue
@@ -5,10 +5,9 @@
       class="row p-4 pb-0 pe-lg-0 pt-lg-5 pb-lg-5 pe-lg-5 align-items-center rounded-3 border shadow-lg"
     >
       <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-        <h1 class="display-4 fw-bold lh-1">{{ name }}</h1>
-        <b>Breed:</b>
+        <h1 class="display-4 fw-bold lh-1">{{ title }}</h1>
         <p class="lead">
-          {{ breed }}
+          {{ period }}
         </p>
         <b>Description:</b>
         <p class="lead">
@@ -16,7 +15,7 @@
         </p>
         <b>Location:</b>
         <p class="lead">
-          {{ location.name }} - {{location.city}}
+          {{ poi.location }}
         </p>
         <div
           class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3"
@@ -26,12 +25,12 @@
             class="btn btn-outline-secondary btn-lg px-4"
             @click="backToList"
           >
-            Back to list
+            Back to all events
           </button>
         </div>
       </div>
       <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
-        <img class="rounded-lg-3" :src="img" alt="" width="" />
+        <img class="rounded-lg-3" :src="require('@/assets/' + pictures[1].url)" alt="" width="" />
       </div>
     </div>
   </div>
@@ -49,13 +48,13 @@ export default {
 
   async asyncData({ route, $axios }) {
     const { id } = route.params
-    const { data } = await $axios.get('/api/cats/' + id)
+    const { data } = await $axios.get('/api/events/' + id)
     return {
-      name: data.name,
-      breed: data.breed,
-      img: data.img,
+      title: data.title,
+      period: data.period,
+      pictures: data.picture,
       description: data.description,
-      location: data.location,
+      poi: data.poi,
 
       crumbs: [{
         name: 'Home',
@@ -66,14 +65,14 @@ export default {
         path: '/events',
       },
       {
-        name: data.name,
+        name: data.title,
         path: '/events/' + id,
       }]
     }
   },
   head(){
     return {
-      title: this.name
+      title: this.title
     }
   },
   mounted(){
--- server/api.js
+++ server/api.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-// const { Sequelize, DataTypes } = require("sequelize")
-// const initialize = require('./initialize').default
+const { Sequelize, DataTypes } = require("sequelize")
+const initialize = require('./initialize').default
 app.use(express.json())
 
 // Development
-// const database = new Sequelize("postgres://postgres:postgres@localhost:5432/hyp")
+const database = new Sequelize("postgres://postgres:postgres@localhost:5432/hyp")
 
 // Production (use this code when deploying to production in Heroku)
 // const pg = require('pg')
@@ -18,90 +18,91 @@ app.use(express.json())
 
 
 // Function that will initialize the connection to the database
-// async function initializeDatabaseConnection() {
-//     await database.authenticate()
-//     const Cat = database.define("cat", {
-//         name: DataTypes.STRING,
-//         description: DataTypes.STRING,
-//         breed: DataTypes.STRING,
-//         img: DataTypes.STRING,
-//     })
-//     const Location  = database.define("location", {
-//         name: DataTypes.STRING,
-//         city: DataTypes.STRING,
-//     })
+async function initializeDatabaseConnection() {
+    await database.authenticate()
+    const Cat = database.define("cat", {
+        name: DataTypes.STRING,
+        description: DataTypes.STRING,
+        breed: DataTypes.STRING,
+        img: DataTypes.STRING,
+    })
+    const Location  = database.define("location", {
+        name: DataTypes.STRING,
+        city: DataTypes.STRING,
+    })
 
-//     /*
-//     const Event = database.define("event", {
-//         title: DataTypes.STRING,
-//         info: DataTypes.STRING,
-//         description: DataTypes.STRING,
-//         //poi: DataTypes.INTEGER,
-//     })
-//     const PoI = database.define("poi", {
-//         title: DataTypes.STRING,
-//         info: DataTypes.STRING,
-//         description: DataTypes.STRING,
-//         coordinates: DataTypes.STRING,
-//     })
-//     const Itinerary = database.define("itinerary", {
-//         title: DataTypes.STRING,
-//         duration: DataTypes.STRING,
-//         description: DataTypes.STRING,
-//     })
-//     const Service = database.define("service", {
-//         name: DataTypes.STRING,
-//         //type: DataTypes.STRING,
-//         address: DataTypes.STRING,
-//         openhours: DataTypes.STRING,
-//     })
-//     const ServiceType = database.define("servicetype", {
-//         name: DataTypes.STRING,
-//         description: DataTypes.STRING,
-//         image: DataTypes.STRING,
-//     })
-//     const Picture = database.define("picture", {
-//         url: DataTypes.STRING,
-//     })
-//     const Poi_Itinerary = database.define("poi_itinerary", {
-//         order: DataTypes.INTEGER,
-//     })
-//     */
+    /*
+    const Event = database.define("event", {
+        title: DataTypes.STRING,
+        //info: DataTypes.STRING,
+        description: DataTypes.STRING,
+        period: DataTypes.STRING,
+    })
+    const PoI = database.define("poi", {
+        title: DataTypes.STRING,
+        info: DataTypes.STRING,
+        description: DataTypes.STRING,
+        coordinates: DataTypes.STRING,
+    })
+    const Itinerary = database.define("itinerary", {
+        title: DataTypes.STRING,
+        duration: DataTypes.STRING,
+        description: DataTypes.STRING,
+    })
+    const Service = database.define("service", {
+        name: DataTypes.STRING,
+        //type: DataTypes.STRING,
+        address: DataTypes.STRING,
+        openhours: DataTypes.STRING,
+    })
+    const ServiceType = database.define("servicetype", {
+        name: DataTypes.STRING,
+        description: DataTypes.STRING,
+        image: DataTypes.STRING,
+    })
+    const Picture = database.define("picture", {
+        url: DataTypes.STRING,
+        //caption: DataTypes.STRING,
+    })
+    const Poi_Itinerary = database.define("poi_itinerary", {
+        order: DataTypes.INTEGER,
+    })
+    */
 
-//     Location.hasMany(Cat)
-//     Cat.belongsTo(Location)
+    Location.hasMany(Cat)
+    Cat.belongsTo(Location)
     
-//     /*
-//     Event.belongsTo(PoI);
-//     PoI.hasMany(Event);
+    /*
+    Event.belongsTo(PoI);
+    PoI.hasMany(Event);
 
-//     Itinerary.belongsToMany(PoI, {through: Poi_Itinerary});
-//     PoI.belongsToMany(Itinerary, {through: Poi_Itinerary});
+    Itinerary.belongsToMany(PoI, {through: Poi_Itinerary});
+    PoI.belongsToMany(Itinerary, {through: Poi_Itinerary});
 
-//     Service.belongsTo(ServiceType);
-//     ServiceType.hasMany(Service);
+    Service.belongsTo(ServiceType);
+    ServiceType.hasMany(Service);
 
-//     Picture.belongsTo(Event);
-//     Event.hasMany(Picture);
-//     Picture.belongsTo(PoI);
-//     PoI.hasMany(Picture);
-//     */
+    Picture.belongsTo(Event);
+    Event.hasMany(Picture);
+    Picture.belongsTo(PoI);
+    PoI.hasMany(Picture);
+    */
 
-//     await database.sync({ force: true })
-//     return {
-//         Cat,
-//         Location,
-//         /*
-//         Event,
-//         Picture,
-//         Itinerary,
-//         PoI,
-//         Poi_Itinerary,
-//         Service,
-//         ServiceType
-//         */
-//     }
-// }
+    await database.sync({ force: true })
+    return {
+        Cat,
+        Location,
+        /*
+        Event,
+        Picture,
+        Itinerary,
+        PoI,
+        Poi_Itinerary,
+        Service,
+        ServiceType
+        */
+    }
+}
 
 // With this line, our server will know how to parse any incoming request
 // that contains some JSON in the body
@@ -126,6 +127,57 @@ const pageContentObject = {
     },
 }
 
+const eventDetails =[
+    {
+        title: "2022 Winter Festival",
+        period: "October 10 - March 21",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et tincidunt elit, in finibus elit. Aliquam nec posuere sem, at faucibus erat. Suspendisse iaculis lorem id odio placerat bibendum. Suspendisse potenti. Sed quis efficitur erat. Pellentesque non velit ipsum. Maecenas finibus felis a magna auctor finibus. Mauris tincidunt nibh sit amet ante consectetur, non cursus elit feugiat.
+        Integer vitae elit at nunc lacinia egestas. Etiam nec sagittis lorem. Phasellus consectetur mauris eget neque posuere, vitae sagittis massa congue. Etiam vitae eleifend odio, sit amet tempus ex. Ut semper feugiat erat, id consequat elit volutpat sed. Curabitur vel arcu at risus vehicula blandit in ut nunc. In nec pellentesque tellus. Maecenas vitae purus lacinia, tristique elit vitae, interdum est. Ut feugiat nulla et vestibulum efficitur. Suspendisse potenti. Duis ex dolor, vestibulum a leo eu, dapibus elementum ipsum. Curabitur euismod rhoncus nulla ac interdum. Mauris vulputate viverra scelerisque. Mauris ullamcorper tempus eros.`,
+        poi: {
+            id: 1,
+            title: "Central Plaza",
+            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et tincidunt elit, in finibus elit. Aliquam nec posuere sem, at faucibus erat. Suspendisse iaculis lorem id odio placerat bibendum. Suspendisse potenti. Sed quis efficitur erat. Pellentesque non velit ipsum. Maecenas finibus felis a magna auctor finibus. Mauris tincidunt nibh sit amet ante consectetur, non cursus elit feugiat.
+            Integer vitae elit at nunc lacinia egestas. Etiam nec sagittis lorem. Phasellus consectetur mauris eget neque posuere, vitae sagittis massa congue. Etiam vitae eleifend odio, sit amet tempus ex. Ut semper feugiat erat, id consequat elit volutpat sed. Curabitur vel arcu at risus vehicula blandit in ut nunc. In nec pellentesque tellus. Maecenas vitae purus lacinia, tristique elit vitae, interdum est. Ut feugiat nulla et vestibulum efficitur. Suspendisse potenti. Duis ex dolor, vestibulum a leo eu, dapibus elementum ipsum. Curabitur euismod rhoncus nulla ac interdum. Mauris vulputate viverra scelerisque. Mauris ullamcorper tempus eros.`,    
+            location: "Location X",
+            picture: {
+                url: "poi.png"
+            }
+        },
+        picture: [
+            {
+                url: "cover.png"
+            },
+            {
+                url: "event.png"
+            }
+        ]
+    },
+    {
+        title: "Event 2",
+        period: "May 24, 5.30-6.00 P. M.",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et tincidunt elit, in finibus elit. Aliquam nec posuere sem, at faucibus erat. Suspendisse iaculis lorem id odio placerat bibendum. Suspendisse potenti. Sed quis efficitur erat. Pellentesque non velit ipsum. Maecenas finibus felis a magna auctor finibus. Mauris tincidunt nibh sit amet ante consectetur, non cursus elit feugiat.
+        Integer vitae elit at nunc lacinia egestas. Etiam nec sagittis lorem. Phasellus consectetur mauris eget neque posuere, vitae sagittis massa congue. Etiam vitae eleifend odio, sit amet tempus ex. Ut semper feugiat erat, id consequat elit volutpat sed. Curabitur vel arcu at risus vehicula blandit in ut nunc. In nec pellentesque tellus. Maecenas vitae purus lacinia, tristique elit vitae, interdum est. Ut feugiat nulla et vestibulum efficitur. Suspendisse potenti. Duis ex dolor, vestibulum a leo eu, dapibus elementum ipsum. Curabitur euismod rhoncus nulla ac interdum. Mauris vulputate viverra scelerisque. Mauris ullamcorper tempus eros.`,
+        poi: {
+            id: 2,
+            title: "Place number 2",
+            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et tincidunt elit, in finibus elit. Aliquam nec posuere sem, at faucibus erat. Suspendisse iaculis lorem id odio placerat bibendum. Suspendisse potenti. Sed quis efficitur erat. Pellentesque non velit ipsum. Maecenas finibus felis a magna auctor finibus. Mauris tincidunt nibh sit amet ante consectetur, non cursus elit feugiat.
+            Integer vitae elit at nunc lacinia egestas. Etiam nec sagittis lorem. Phasellus consectetur mauris eget neque posuere, vitae sagittis massa congue. Etiam vitae eleifend odio, sit amet tempus ex. Ut semper feugiat erat, id consequat elit volutpat sed. Curabitur vel arcu at risus vehicula blandit in ut nunc. In nec pellentesque tellus. Maecenas vitae purus lacinia, tristique elit vitae, interdum est. Ut feugiat nulla et vestibulum efficitur. Suspendisse potenti. Duis ex dolor, vestibulum a leo eu, dapibus elementum ipsum. Curabitur euismod rhoncus nulla ac interdum. Mauris vulputate viverra scelerisque. Mauris ullamcorper tempus eros.`,    
+            location: "Location X",
+            picture: {
+                url: "poi.png"
+            }
+        },
+        picture: [
+            {
+                url: "cover.png"
+            },
+            {
+                url: "event.png"
+            }
+        ]
+    }
+]
+
 
 async function runMainApi() {
     const models = await initializeDatabaseConnection()
@@ -142,6 +194,15 @@ async function runMainApi() {
         const result = await models.Cat.findOne({ where: { id }, include: [{model: models.Location}] })
         return res.json(result)
     })
+
+    app.get('/events/:id', async (req, res) => {
+        const id = +req.params.id
+        // const result = await models.Event.findOne({ where: { id }, include: [{model: models.Picture},{model: models.PoI}] })
+        const result = eventDetails[id-1]
+        console.log(result)
+        return res.json(result)
+    })
+
 
     // HTTP GET api that returns all the cats in our actual database
     // app.get("/cats", async (req, res) => {
