698ee109290d861bb10b52bdb3c689f5e509a29f
veris744 <isadede@alum.us.es>
2022-05-26 13:36:41 +0200

    event details poi

--- pages/events/_id.vue
+++ pages/events/_id.vue
@@ -13,15 +13,21 @@
           {{ description }}
         </p>
 
-    <br>
-    <Carousel :slides="[pictures[0].url, pictures[1].url]"/>
-    <br>
-
-        <h2>Location:</h2>
-        <p class="lead">
-          {{ poi.location }}
-        </p>
         <br>
+          <Carousel :slides="[pictures[0].url, pictures[1].url]"/>
+        <br><br>
+
+        <h2>About the place:</h2>
+        <nuxt-link :to="`/pois/${poi.id}`" class="nuxt-clickable">
+          <div class="image-container">
+            <figure>
+               <img :src="require('@/assets/' + poi.picture.url)" alt="" />
+              <figcaption>{{ poi.title }}</figcaption>
+            </figure>
+          </div>
+        </nuxt-link>
+
+        <br><br>
         <div
           class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3"
         >
@@ -43,6 +49,42 @@ h2 {
     font-weight: bolder;
     font-size: 26px;
     font-family: Arial;
+}
+.nuxt-clickable {
+  display: inline-block;
+  width: 100%;
+}
+.image-container {
+  text-align: center;
+}
+figcaption {
+    font-variant: small-caps;
+    font-family: Arial;
+    font-weight: bold;
+    font-size: 22px;
+    line-height: 2;
+    color: #26466F;
+}
+figure {
+    background: #FFFFFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 5px;
+}
+.image-container:hover {
+    transform: scale(1.1);
+    -ms-transform: scale(1.1);
+    -webkit-transform: scale(1.1);
+    -moz-transform: scale(1.1);
+    -o-transform: scale(1.1);
+}
+.image-container {
+    padding: 5%;
+    margin-bottom: -5%;
+
+    transition: transform 0.2s;
+    -webkit-transition: -webkit-transform 0.2s;
+    -moz-transition: -moz-transform 0.2s;
+    -o-transition: -o-transform 0.2s;
 }
 </style>
 
