d2f4fd407a02c9454bc8783c9cce0f7fb90d26b3
gubo97000 <gubo97000@hotmail.it>
2022-06-02 01:01:31 +0200

    + events page is dynamic

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,22 +1,53 @@
 <template>
   <div class="page container mt-5">
     <h1 class="display-4">Events</h1>
-    <Breadcrumb class="row justify-items-center mt-4" :crumbs="crumbs" @selected="selected"/>
+    <Breadcrumb
+      class="row justify-items-center mt-4"
+      :crumbs="crumbs"
+      @selected="selected"
+    />
     <div class="jumbotron">
       <div class="container mt-5">
         <p class="lead">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et tincidunt elit, in finibus elit. Aliquam nec posuere sem, at faucibus erat. Suspendisse iaculis lorem id odio placerat bibendum. Suspendisse potenti. Sed quis efficitur erat. Pellentesque non velit ipsum. Maecenas finibus felis a magna auctor finibus. Mauris tincidunt nibh sit amet ante consectetur, non cursus elit feugiat.
-        Integer vitae elit at nunc lacinia egestas. Etiam nec sagittis lorem. Phasellus consectetur mauris eget neque posuere, vitae sagittis massa congue. Etiam vitae eleifend odio, sit amet tempus ex. Ut semper feugiat erat, id consequat elit volutpat sed. Curabitur vel arcu at risus vehicula blandit in ut nunc. In nec pellentesque tellus. Maecenas vitae purus lacinia, tristique elit vitae, interdum est. Ut feugiat nulla et vestibulum efficitur. Suspendisse potenti. Duis ex dolor, vestibulum a leo eu, dapibus elementum ipsum. Curabitur euismod rhoncus nulla ac interdum. Mauris vulputate viverra scelerisque. Mauris ullamcorper tempus eros.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
+          tincidunt elit, in finibus elit. Aliquam nec posuere sem, at faucibus
+          erat. Suspendisse iaculis lorem id odio placerat bibendum. Suspendisse
+          potenti. Sed quis efficitur erat. Pellentesque non velit ipsum.
+          Maecenas finibus felis a magna auctor finibus. Mauris tincidunt nibh
+          sit amet ante consectetur, non cursus elit feugiat. Integer vitae elit
+          at nunc lacinia egestas. Etiam nec sagittis lorem. Phasellus
+          consectetur mauris eget neque posuere, vitae sagittis massa congue.
+          Etiam vitae eleifend odio, sit amet tempus ex. Ut semper feugiat erat,
+          id consequat elit volutpat sed. Curabitur vel arcu at risus vehicula
+          blandit in ut nunc. In nec pellentesque tellus. Maecenas vitae purus
+          lacinia, tristique elit vitae, interdum est. Ut feugiat nulla et
+          vestibulum efficitur. Suspendisse potenti. Duis ex dolor, vestibulum a
+          leo eu, dapibus elementum ipsum. Curabitur euismod rhoncus nulla ac
+          interdum. Mauris vulputate viverra scelerisque. Mauris ullamcorper
+          tempus eros.
         </p>
-        <br>
+        <br />
 
         <div ref="dropdowns">
-          <Dropdown :title="'ALL YEAR EVENTS'" :index="1" :ref="'dropdownToggler' + i">
+          <Dropdown
+            :ref="'dropdownToggler' + i"
+            :title="'ALL YEAR EVENTS'"
+            :index="1"
+          >
             <div class="column_wrapper">
-              <nuxt-link v-for="item in eventsList" :to="`/events/${item.id}`" class="nuxt-clickable">
+              <nuxt-link
+                v-for="item in events.nodes"
+                :key="item.id"
+                :to="`/events/${item.id}`"
+                class="nuxt-clickable"
+              >
                 <div class="image-container">
                   <figure>
-                    <img :src="require('@/assets/' + item.picture[0].url)" alt="" width="100%"/>
+                    <!-- <img
+                      :src="require('@/assets/' + item.picture[0].url)"
+                      alt=""
+                      width="100%"
+                    /> -->
                     <figcaption>{{ item.title }}</figcaption>
                   </figure>
                 </div>
@@ -26,19 +57,25 @@
 
           <Dropdown :title="'WINTER EVENTS'" :index="2">
             <div class="column_wrapper">
-              <nuxt-link v-for="item in eventsList" :to="`/events/${item.id}`" class="nuxt-clickable">
-                <div v-if="(item.period.includes('October') || item.period.includes('November') ||
-                item.period.includes('December') || item.period.includes('January') ||
-                item.period.includes('February') || item.period.includes('March')) && 
-                !(item.period.includes('April') || item.period.includes('May') ||
-                item.period.includes('June') || item.period.includes('July') ||
-                item.period.includes('August') || item.period.includes('September'))">
+              <nuxt-link
+                v-for="item in events.nodes"
+                v-if="!isSummer(item.info) && isWinter(item.info)"
+                :key="`S${item.id}`"
+                :to="`/events/${item.id}`"
+                class="nuxt-clickable"
+              >
+                <div
+                >
                   <div class="image-container">
-                  <figure>
-                    <img :src="require('@/assets/cover.png')" alt="" width="100%"/>
-                    <figcaption>{{ item.title }}</figcaption>
-                  </figure>
-                </div>
+                    <figure>
+                      <img
+                        :src="require('@/assets/cover.png')"
+                        alt=""
+                        width="100%"
+                      />
+                      <figcaption>{{ item.title }}</figcaption>
+                    </figure>
+                  </div>
                 </div>
               </nuxt-link>
             </div>
@@ -46,37 +83,137 @@
 
           <Dropdown :title="'SUMMER EVENTS'" :index="3">
             <div class="column_wrapper">
-              <nuxt-link v-for="item in eventsList" :to="`/events/${item.id}`" class="nuxt-clickable">
-                <div v-if="(item.period.includes('April') || item.period.includes('May') ||
-                item.period.includes('June') || item.period.includes('July') ||
-                item.period.includes('August') || item.period.includes('September')) &&
-                !(item.period.includes('October') || item.period.includes('November') ||
-                item.period.includes('December') || item.period.includes('January') ||
-                item.period.includes('February') || item.period.includes('March'))">
+              <nuxt-link
+                v-for="item in events.nodes"
+                v-if="isSummer(item.info) && !isWinter(item.info)"
+                :key="`W${item.id}`"
+                :to="`/events/${item.id}`"
+                class="nuxt-clickable"
+              >
+                <div>
                   <div class="image-container">
-                  <figure>
-                    <img :src="require('@/assets/cover.png')" alt="" width="100%"/>
-                    <figcaption>{{ item.title }}</figcaption>
-                  </figure>
-                </div>
+                    <figure>
+                      <img
+                        :src="require('@/assets/cover.png')"
+                        alt=""
+                        width="100%"
+                      />
+                      <figcaption>{{ item.title }}</figcaption>
+                    </figure>
+                  </div>
                 </div>
               </nuxt-link>
             </div>
           </Dropdown>
         </div>
-        
-        <br>
+
+        <br />
       </div>
     </div>
-    <br><br>
+    <br /><br />
   </div>
 </template>
 
+<script>
+import { gql } from 'graphql-tag'
+import Breadcrumb from '~/components/Breadcrumb.vue'
+import Dropdown from '~/components/Dropdown.vue'
+
+let dropdowns
+
+export default {
+  name: 'Events',
+  components: {
+    Breadcrumb,
+    Dropdown,
+  },
+  data() {
+    return {
+      events: {},
+      crumbs: [
+        {
+          name: 'Home',
+          path: '/',
+        },
+        {
+          name: 'Events',
+          path: '/events',
+        },
+      ],
+    }
+  },
+  head() {
+    return {
+      title: 'Events',
+    }
+  },
+  apollo: {
+    events: {
+      // IMPORTANT: the name of the variable MUST be the same of the entity in the GraphQL query
+      // prefetch: true,
+      query: gql`
+        query MyQuery {
+          events {
+            nodes {
+              id
+              title
+              info
+            }
+          }
+        }
+      `,
+      // fetchPolicy: 'network-only',
+    },
+  },
+  mounted() {
+    dropdowns = this.$refs.dropdowns
+
+    // Step 2: When clicking a dropdown, close all the others (if opened)
+    this.$on('closeAllDropdowns', (index) => {
+      for (let i = 1; i <= dropdowns.children.length; i++)
+        if (index != i) this.$emit('closeDropdown')
+    })
+  },
+  methods: {
+    isSummer: (info) => {
+      if (
+        info.includes('April') ||
+        info.includes('May') ||
+        info.includes('June') ||
+        info.includes('July') ||
+        info.includes('August') ||
+        info.includes('September')
+      )
+        return true
+      else return false
+    },
+    isWinter: (info) => {
+      if (
+        info.includes('October') ||
+        info.includes('November') ||
+        info.includes('December') ||
+        info.includes('January') ||
+        info.includes('February') ||
+        info.includes('March')
+      )
+        return true
+      else return false
+    },
+  },
+  // async asyncData({ $axios }) {
+  //   const { data } = await $axios.get('/api/events')
+  //   return {
+  //     eventsList: data,
+  //   }
+  // },
+}
+</script>
+
 <style scoped>
 h2 {
-    font-weight: bolder;
-    font-size: 26px;
-    font-family: Arial;
+  font-weight: bolder;
+  font-size: 26px;
+  font-family: Arial;
 }
 
 .column_wrapper {
@@ -90,80 +227,32 @@ h2 {
   text-align: center;
 }
 figcaption {
-    font-variant: small-caps;
-    font-family: Arial;
-    font-weight: bold;
-    font-size: 22px;
-    line-height: 2;
-    color: #26466F;
+  font-variant: small-caps;
+  font-family: Arial;
+  font-weight: bold;
+  font-size: 22px;
+  line-height: 2;
+  color: #26466f;
 }
 figure {
-    background: #FFFFFF;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 5px;
+  background: #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
 }
 .image-container:hover {
-    transform: scale(1.1);
-    -ms-transform: scale(1.1);
-    -webkit-transform: scale(1.1);
-    -moz-transform: scale(1.1);
-    -o-transform: scale(1.1);
+  transform: scale(1.1);
+  -ms-transform: scale(1.1);
+  -webkit-transform: scale(1.1);
+  -moz-transform: scale(1.1);
+  -o-transform: scale(1.1);
 }
 .image-container {
-    padding: 5%;
-    margin-bottom: -5%;
+  padding: 5%;
+  margin-bottom: -5%;
 
-    transition: transform 0.2s;
-    -webkit-transition: -webkit-transform 0.2s;
-    -moz-transition: -moz-transform 0.2s;
-    -o-transition: -o-transform 0.2s;
+  transition: transform 0.2s;
+  -webkit-transition: -webkit-transform 0.2s;
+  -moz-transition: -moz-transform 0.2s;
+  -o-transition: -o-transform 0.2s;
 }
 </style>
-
-<script>
-import Breadcrumb from '~/components/Breadcrumb.vue'
-import Dropdown from '~/components/Dropdown.vue'
-
-let dropdowns;
-
-export default {
-  name: 'Events',
-  components: {
-    Breadcrumb,
-    Dropdown
-},
-  head(){
-    return {
-      title: "Events"
-    }
-  },
-  data() {
-    return {
-      crumbs: [{
-        name: 'Home',
-        path: '/',
-      },
-      {
-        name: 'Events',
-        path: '/events',
-      }]
-    }
-  },
-  mounted() {
-    dropdowns = this.$refs.dropdowns;
-
-    // Step 2: When clicking a dropdown, close all the others (if opened)
-    this.$on('closeAllDropdowns', (index) => {
-      for(var i = 1; i <= dropdowns.children.length; i++)
-        if( index != i )
-          this.$emit('closeDropdown');
-    })
-  },
-  async asyncData({ $axios }) {
-    const { data } = await $axios.get('/api/events')
-    return {
-      eventsList: data,
-    }
-  },
-}
-</script>
