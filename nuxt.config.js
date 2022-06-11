9e27156fb8aef8200405af9bffddbd84697a5f62
gubo97000 <gubo97000@hotmail.it>
2022-06-11 10:44:42 +0200

    yet another heroku fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,7 @@ export default {
   apollo: {
     clientConfigs: {
       default: {
-        httpEndpoint: '/api/graphql',
+        httpEndpoint: `http://${process.env.HEROKU_APP_NAME}.herokuapp.com:${process.env.PORT}/api/graphql`,
       }
     }
   },
