3b53cb2a34b456aa6da540da3d809ff62ce4e4ca
gubo97000 <gubo97000@hotmail.it>
2022-06-11 10:34:39 +0200

    heroku fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,7 @@ export default {
   apollo: {
     clientConfigs: {
       default: {
-        httpEndpoint: 'http://localhost/api/graphql',
+        httpEndpoint: '/api/graphql',
       }
     }
   },
