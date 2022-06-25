47b7fb96e2064932524ee12099a641346928b0ed
Alessandro Atanassov <alessandro.atanassov@mail.polimi.it>
2022-06-25 22:20:44 +0200

    Remove duplicate style code in Breadcrumb

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -6,7 +6,7 @@
             <div v-for="(crumb, ci) in crumbs" :key="ci" class="crumb-item">
                 <!-- Crumb item (actual link) -->
                 <li>
-                    <nuxt-link :class="{'disabled-link': isLast(ci)}" :to="crumb.path">
+                    <nuxt-link :class="{ 'disabled-link': isLast(ci) }" :to="crumb.path">
                         <div class="btn btn-link hover:text-blue-300" :class="{ disabled: isLast(ci) }">
                             {{ crumb.name }}
                         </div>
@@ -14,7 +14,7 @@
                 </li>
                 <!-- Separator between items (except for last item) -->
                 <li>
-                    <div v-if="!isLast(ci)" disabled="isLast(ci)" class="separator" >
+                    <div v-if="!isLast(ci)" disabled="isLast(ci)" class="separator">
                         {{ "&#187;" }}
                     </div>
                 </li>
@@ -25,19 +25,19 @@
 
 <script>
 export default {
-  name: 'Breadcrumb',
-  props: {
-    crumbs: {
-        type: Array,
-        required: true
-    }
-  },
-  methods: {
-    // True if lowest item in the hierarchy of crumbs (i.e. the currently visited page)
-    isLast(index) {
-        return index === this.crumbs.length - 1;
+    name: 'Breadcrumb',
+    props: {
+        crumbs: {
+            type: Array,
+            required: true
+        }
     },
-  }
+    methods: {
+        // True if lowest item in the hierarchy of crumbs (i.e. the currently visited page)
+        isLast(index) {
+            return index === this.crumbs.length - 1;
+        },
+    }
 }
 </script>
 
@@ -46,51 +46,29 @@ export default {
 @media screen and (min-width: 600px) {
     .breadcrumb {
         background-color: white;
-        border: 1px solid rgba(0,0,0,0.125);
+        border: 1px solid rgba(0, 0, 0, 0.125);
         border-radius: 0.37rem;
         width: auto;
     }
 }
+
 /* Portrait mode */
 @media screen and (max-width: 600px) {
     .breadcrumb {
         justify-content: center;
     }
 }
+
 .separator {
     font-size: 22px;
     color: #6c757d;
 }
+
 .crumb-item {
     display: flex;
 }
-.disabled-link {
-   pointer-events: none
- }
- </style>
 
-<style scoped>
-@media screen and (min-width: 600px) {
-  .breadcrumb {
-    background-color: white;
-    border: 1px solid rgba(0, 0, 0, 0.125);
-    border-radius: 0.37rem;
-    width: auto;
-  }
-}
-@media screen and (max-width: 600px) {
-  .breadcrumb {
-    justify-content: center;
-  }
-}
-.separator {
-  font-size: 22px;
-  color: #6c757d;
-}
-.crumb-item {
-  display: flex;
-}
 .disabled-link {
-  pointer-events: none;
+    pointer-events: none
 }
 </style>
