326951d70de179fe69abbd7688e72f0d086d827a
Alessandro Atanassov <alessandro.atanassov@mail.polimi.it>
2022-06-28 21:18:20 +0200

    Hide controls from carousels with only one slide

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -4,14 +4,17 @@
     <!-- Carousel indicators (the set of dots at the bottom that controls the current slide) -->
     <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
       <!-- One "dot" per slide -->
-      <button v-for="(slide, si) in slides" :key="si" type="button" data-bs-target="#carouselExampleCaptions"
+      <button
+      v-for="(slide, si) in slides"
+      v-if="slides.length > 1" :key="si" type="button" data-bs-target="#carouselExampleCaptions"
         class="control" :data-bs-slide-to="si" :class="{ active: isActive(si) }" aria-current="true"
         aria-label="Slide 1"></button>
     </div>
 
     <!-- Actual slide images (with captions) -->
     <div class="carousel-inner relative w-full overflow-hidden">
-      <div v-for="(slide, si) in slides" :key="si" class="carousel-item relative float-left img-container"
+      <div
+        v-for="(slide, si) in slides" :key="si" class="carousel-item relative float-left img-container"
         :class="{ active: isActive(si) }">
         <!-- Image -->
         <img :src="require('@/assets/' + slide)" class="block carousel-img" alt="..." />
@@ -25,6 +28,7 @@
 
     <!-- Button "previous slide" -->
     <button
+      v-if="slides.length > 1"
       class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0 btn-left"
       type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
       <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
@@ -33,6 +37,7 @@
 
     <!-- Button "next slide" -->
     <button
+      v-if="slides.length > 1"
       class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0 btn-right"
       type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
       <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
