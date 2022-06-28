f92d577f650c62f951d6b6ef84acaba0e155ffb7
Alessandro Atanassov <alessandro.atanassov@mail.polimi.it>
2022-06-28 22:59:35 +0200

    Add titles to iframes

--- pages/about.vue
+++ pages/about.vue
@@ -33,6 +33,7 @@
         <h3 class="section-title">HOW TO REACH US</h3>
         <div class="howtoreach-container">
           <iframe
+            title="How to reach Towny"
             src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d22385.345367822818!2d9.183978311645502!3d45.46641612838561!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sit!4v1652736022694!5m2!1sen!2sit"
             width="35%" style="border: 0" allowfullscreen="" loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"></iframe>
